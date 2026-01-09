238f47aebec5f29579e03d352356923c759b41c4
Samuraininja84 <158503209+samuraininja84@users.noreply.github.com>
2026-01-09 13:09:41 -0500

    Refactor LabelListItem for improved readability

--- Editor/Lists/Items/LabelListItem.cs
+++ Editor/Lists/Items/LabelListItem.cs
@@ -1,65 +1,86 @@
-﻿using Aarthificial.Typewriter.Editor.Extensions;
+using UnityEngine;
+using UnityEngine.UIElements;
 using UnityEditor;
 using UnityEditor.UIElements;
-using UnityEngine.UIElements;
+using Aarthificial.Typewriter.Editor.Extensions;
 
-namespace Aarthificial.Typewriter.Editor.Lists.Items {
-  public class LabelListItem : EditableListItem {
-    protected readonly Label Label;
-    protected readonly VisualElement Root;
-    protected readonly TextField Text;
-    protected readonly Label Type;
+namespace Aarthificial.Typewriter.Editor.Lists.Items
+{
+    public class LabelListItem : EditableListItem
+    {
+        protected readonly Label Label;
+        protected readonly VisualElement Root;
+        protected readonly TextField Text;
+        protected readonly Label Type;
 
-    public LabelListItem() {
-      Root = new VisualElement();
-      Root.AddToClassList("editable-item");
+        private bool isEditing => Text.value == Label.text;
 
-      Label = new Label();
-      Label.AddToClassList("editable-item__label");
-      Text = new TextField { style = { display = DisplayStyle.None } };
-      Text.AddToClassList("editable-item__field");
+        private bool Hovered => Root.worldBound.Contains(Event.current.mousePosition);
 
-      Type = new Label();
-      Type.AddToClassList("editable-item__type");
+        public LabelListItem()
+        {
+            Root = new VisualElement();
+            Root.AddToClassList("editable-item");
 
-      Label.RegisterCallback<MouseDownEvent>(HandleMouseDown);
-      RegisterCallback<FocusOutEvent>(HandleFocusOut);
+            Label = new Label();
+            Label.AddToClassList("editable-item__label");
+            Text = new TextField { style = { display = DisplayStyle.None } };
+            Text.AddToClassList("editable-item__field");
 
-      Add(Root);
-      Root.Add(Label);
-      Root.Add(Text);
-      Root.Add(Type);
+            Type = new Label();
+            Type.AddToClassList("editable-item__type");
+
+            Label.RegisterCallback<MouseDownEvent>(HandleMouseDown);
+            RegisterCallback<FocusOutEvent>(HandleFocusOut);
+
+            Add(Root);
+            Root.Add(Label);
+            Root.Add(Text);
+            Root.Add(Type);
+        }
+
+        protected void SetLabel(string text) => Label.text = string.IsNullOrEmpty(text) ? "<empty>" : text;
+
+        protected virtual void HandleFocusOut(FocusOutEvent evt)
+        {
+            // Check if we selected something else outside of this item
+            if (Hovered && isEditing) return;
+
+            Label.style.display = DisplayStyle.Flex;
+            SetLabel(Text.value);
+            Text.style.display = DisplayStyle.None;
+        }
+
+        private void HandleMouseDown(MouseDownEvent evt)
+        {
+            // Double click - enter edit mode
+            if (evt.clickCount == 2)
+            {
+                // Focus the text field
+                Label.style.display = DisplayStyle.None;
+                Text.style.display = DisplayStyle.Flex;
+                Text.Focus();
+
+                // Select the text
+                Text.SelectAll();
+            }
+        }
+
+        public override void BindProperty(SerializedProperty property)
+        {
+            var child = property.FirstString();
+            if (child != null)
+            {
+                SetLabel(child.stringValue);
+                Text.BindProperty(child);
+            }
+        }
+
+        public override void Unbind()
+        {
+            SetLabel("");
+            Text.Unbind();
+        }
     }
-
-    protected void SetLabel(string text) {
-      Label.text = string.IsNullOrEmpty(text) ? "<empty>" : text;
-    }
-
-    protected virtual void HandleFocusOut(FocusOutEvent evt) {
-      Label.style.display = DisplayStyle.Flex;
-      SetLabel(Text.value);
-      Text.style.display = DisplayStyle.None;
-    }
-
-    private void HandleMouseDown(MouseDownEvent evt) {
-      if (evt.clickCount == 2) {
-        Label.style.display = DisplayStyle.None;
-        Text.style.display = DisplayStyle.Flex;
-        Text.Focus();
-      }
-    }
-
-    public override void BindProperty(SerializedProperty property) {
-      var child = property.FirstString();
-      if (child != null) {
-        SetLabel(child.stringValue);
-        Text.BindProperty(child);
-      }
-    }
-
-    public override void Unbind() {
-      SetLabel("");
-      Text.Unbind();
-    }
-  }
 }
+
